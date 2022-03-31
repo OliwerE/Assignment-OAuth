f238f30804ba3d75b23ee85304430c8cce89d2a4
Oliwer <oe222ez@student.lnu.se>
2022-03-31 19:38:26 +0200

    Configure Express

---
+++ src/routes/router.js
@@ -0,0 +1,13 @@
+/**
+ * Main router module.
+ */
+import express from 'express'
+import createError from 'http-errors'
+
+export const router = express.Router()
+
+router.get('/', (req, res, next) => { res.render('body/test') })
+
+router.use('*', (req, res, next) => {
+  next(createError(404))
+})
---
+++ src/server.js
@@ -0,0 +1,46 @@
+/**
+ * Express server module.
+ */
+
+import express from 'express'
+import helmet from 'helmet'
+import logger from 'morgan'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import hbs from 'express-hbs'
+import { router } from './routes/router.js'
+
+function run () {
+  const app = express()
+  app.use(helmet())
+  app.use(logger('dev'))
+
+
+  const fullDirName = dirname(fileURLToPath(import.meta.url))
+  app.engine('hbs', hbs.express4({
+    defaultLayout: join(fullDirName, 'views', 'layouts', 'default'),
+    partialsDir: join(fullDirName, 'views', 'partials')
+  }))
+  app.set('view engine', 'hbs')
+  app.set('views', join(fullDirName, 'views'))
+  app.use(express.urlencoded({ extended: false }))
+  app.use(express.static(join(fullDirName, '..', 'public')))
+
+  app.use('/', router)
+
+  app.use((err, req, res, next) => {
+    // if (err.status === 404) {
+    //   return res.status(404).sendFile(join(fullDirName, 'views', 'errors', '404.html'))
+    // }
+
+    // if (err.status === 500) {
+    //   return res.status(500).sendFile(join(fullDirName, 'views', 'errors', '500.html'))
+    // }
+  })
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Listens for localhost@${process.env.PORT}`)
+    console.log('ctrl + c to terminate')
+  })
+}
+run()
