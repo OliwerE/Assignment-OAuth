d2b267c2b5d052351c921e64d03d7ff224583870
Oliwer <oe222ez@student.lnu.se>
2022-03-31 20:25:47 +0200

    Create login page

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,7 +6,7 @@ import createError from 'http-errors'
 
 export const router = express.Router()
 
-router.get('/', (req, res, next) => { res.render('body/test') })
+router.get('/', (req, res, next) => { res.render('body/auth/login') })
 
 router.use('*', (req, res, next) => {
   next(createError(404))
